9b9c2f9848939900509da81b0b5a234d38dee559
Carlos W. Gama <carloswgama@gmail.com>
2020-01-25 19:11:22 -0300

    Flatlist

--- src/screen/home.tsx
+++ src/screen/home.tsx
@@ -4,13 +4,14 @@ import { Toolbar } from '../components/toolbar';
 import { Fab } from '../components/fab';
 import Tarefa from '../models/tarefa';
 import { ItemTarefa } from '../components/item-tarefa';
+import { FlatList } from 'react-native-gesture-handler';
 
 export interface AppProps {
     navigation: any; 
 }
 
 export interface AppState { 
-  tarefa:Tarefa;
+  tarefas:Tarefa[];
 }
 
 
@@ -22,7 +23,10 @@ export default class HomeScreen extends React.Component<AppProps, AppState> {
   constructor(props: AppProps) {
     super(props);
     this.state = { 
-      tarefa: new Tarefa('Teste', '20/10/2000')
+      tarefas: [new Tarefa('Tarefa 1', '07/07/2019', '1'),
+               new Tarefa('Tarefa 2', '07/07/2019', '2'),
+               new Tarefa('Tarefa 3', '07/07/2019', '3')
+              ]
     };
   }
 
@@ -33,9 +37,14 @@ export default class HomeScreen extends React.Component<AppProps, AppState> {
           <Toolbar titulo="Home" navigation={this.props.navigation} menu={true} />
           
           {/* LISTA DE TAREFAS */}
-          <ItemTarefa tarefa={this.state.tarefa} onEditar={(tarefa)=>this.props.navigation.navigate('tarefaEdicao', {tarefa})} onExcluir={(id)=>console.log(id)}/>
-          <ItemTarefa tarefa={this.state.tarefa} onEditar={(tarefa)=>this.props.navigation.navigate('tarefaEdicao', {tarefa})} onExcluir={(id)=>console.log(id)}/>
-          <ItemTarefa tarefa={this.state.tarefa} onEditar={(tarefa)=>this.props.navigation.navigate('tarefaEdicao', {tarefa})} onExcluir={(id)=>console.log(id)}/>
+            <FlatList 
+                data={this.state.tarefas}
+                extraData={this.state.tarefas}
+                keyExtractor={(t) => t.id}
+                renderItem={({item}) => (
+                    <ItemTarefa tarefa={item} onEditar={(tarefa)=>this.props.navigation.navigate('tarefaEdicao', {tarefa})} onExcluir={(id)=>console.log(id)}/>
+                )}    
+              /> 
           {/* LISTA DE TAREFAS [FIM] */}
           
           <Fab onPress={() => this.props.navigation.navigate('tarefaEdicao')} />
